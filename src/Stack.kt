dd89cfb1ccdb2d7decb490cebcd3ca2c07db2413
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-13 19:20:10 -0500

    Solved Stack Q2

--- src/Stack.kt
+++ src/Stack.kt
@@ -1,18 +1,18 @@
 import java.util.Stack
+import java.util.stream.Gatherer
 
 fun buildArray(target: IntArray, n: Int): List<String> {
     var counter = 1
     val ans = arrayListOf<String>()
     val stack = Stack<Int>()
-    for (i in 1..n){
+    for (i in 1..n) {
 
         if (stack.toIntArray().contentEquals(target))
             return ans
         if (target.contains(i)) {
             ans.add("Push")
             stack.push(i)
-        }
-        else{
+        } else {
             ans.add("Push")
             ans.add("Pop")
         }
@@ -20,3 +20,39 @@ fun buildArray(target: IntArray, n: Int): List<String> {
     }
     return ans
 }
+
+fun evalRPN(tokens: Array<String>): Int {
+    val tokensArrayList = tokens.toMutableList()
+    val stack = Stack<Int>()
+    for (i in tokens){
+        when (i) {
+            "-" -> {
+                val op1 = stack.pop()
+                val op2 = stack.pop()
+                stack.push(op2 - op1)
+            }
+            "/" -> {
+                val op1 = stack.pop()
+                val op2 = stack.pop()
+                stack.push(op2 / op1)
+            }
+            "*" -> {
+                val op1 = stack.pop()
+                val op2 = stack.pop()
+                stack.push(op2 * op1)
+            }
+            "+" -> {
+                val op1 = stack.pop()
+                val op2 = stack.pop()
+                stack.push(op2 + op1)
+            }
+            else ->{
+                stack.push(i.toInt())
+            }
+        }
+    }
+    return stack.pop()
+}
+
+
+
