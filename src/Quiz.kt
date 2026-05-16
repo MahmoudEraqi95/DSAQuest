abf95c64b4e8492feecf1b146b1bebc50ee37238
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-15 20:11:01 -0500

    Solved Quiz Q2

--- src/Quiz.kt
+++ src/Quiz.kt
@@ -1,3 +1,5 @@
+import java.util.Stack
+
 fun plusOne(digits: IntArray): IntArray {
     val n = digits.size
     if(digits[n-1] == 9){
@@ -22,4 +24,38 @@ fun plusOne(digits: IntArray): IntArray {
 
     digits[n-1] += 1
     return digits
+}
+
+fun validMountainArray(arr: IntArray): Boolean {
+    if (arr.size<3)
+        return false
+
+    var p1 = 1
+    while (p1<arr.size){
+        if (arr[p1]>arr[p1-1]) {
+            p1++
+            continue
+
+        }
+        else if(arr[p1] == arr[p1-1])
+            return false
+        else{
+            break
+        }
+
+    }
+    if (p1>arr.size-2)
+        return false
+
+    while (p1 <arr.size-1){
+        if (arr[p1]>arr[p1+1]) {
+            p1++
+            continue
+        }
+        else{
+            return false
+        }
+    }
+    return true
+
 }
