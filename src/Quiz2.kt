2dc264af67550c00276ef252c7d49faf2daa934f
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-15 21:47:54 -0500

    Solved Quiz2 Q1

---
+++ src/Quiz2.kt
@@ -0,0 +1,21 @@
+import java.util.Stack
+
+fun removeDuplicateLetters(s: String): String {
+    val lastIndex = mutableMapOf<Char, Int>()
+    val visited = mutableSetOf<Char>()
+    for (i in s.indices){
+        lastIndex[s[i]] = i
+    }
+    val stack = Stack<Char>()
+    for(i in s.indices){
+        val current = s[i]
+        while (stack.isNotEmpty()
+            && stack.last() > current
+            && lastIndex[stack.last()]!! > i){
+            visited.remove(stack.pop())
+        }
+        stack.push(current)
+        visited.add(current)
+    }
+    return stack.joinToString()
+}
