2f1ad29a5e912b5b47b9086a1cd7dcdf84ac27d5
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-17 10:09:01 -0500

    Solved Heap Q2

--- src/SequenceValley/Heap.kt
+++ src/SequenceValley/Heap.kt
@@ -2,6 +2,7 @@ package SequenceValley
 
 import java.util.PriorityQueue
 import kotlin.math.abs
+import kotlin.math.min
 
 fun lastStoneWeight(stones: IntArray): Int {
     val heap = PriorityQueue<Int>(compareByDescending { it  })
@@ -18,4 +19,25 @@ fun lastStoneWeight(stones: IntArray): Int {
         return heap.poll()
     else
         return 0
+}
+
+fun kSmallestPairs(nums1: IntArray, nums2: IntArray, k: Int): List<List<Int>> {
+    val minHeap = PriorityQueue<IntArray>(compareBy{it[0]})
+    val ans = mutableListOf<List<Int>>()
+    var kCopy = k
+    for( i in 0..minOf(nums1.size-1, k)){
+        minHeap.add(intArrayOf(nums1[i]+nums2[0], i, 0))
+    }
+
+    while (kCopy>0){
+        val min = minHeap.poll()
+        val i = min[1]
+        val j = min[2]
+        ans.add(listOf(nums1[i], nums2[j]))
+        if ( j +1 <nums2.size)
+            minHeap.add(intArrayOf(nums1[i] + nums2[j+1], i, j+1))
+        kCopy--
+
+    }
+    return ans
 }
