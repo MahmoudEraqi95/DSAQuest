a5c866cb223180a7d096a88b82e3a0b7800fef50
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-18 23:27:56 -0500

    Solved LinkedList Q2

--- src/AssociationSlope/LinkedList.kt
+++ src/AssociationSlope/LinkedList.kt
@@ -8,12 +8,34 @@ fun deleteDuplicates(head: ListNode?): ListNode? {
     val start = ListNode(0)
     var newHead = head
     start.next = newHead
-    while (newHead?.next != null){
-        if (newHead?.next!!.`val` == newHead.`val`){
+    while (newHead?.next != null) {
+        if (newHead?.next!!.`val` == newHead.`val`) {
             newHead.next = newHead.next!!.next
-        }else{
+        } else {
             newHead = newHead.next
         }
     }
     return start.next
+}
+
+fun oddEvenList(head: ListNode?): ListNode? {
+    var oddList = ListNode(0)
+    var newOddList = oddList
+    var evenList = ListNode(0)
+    var newEvenList = evenList
+    var newHead = head
+    while (newHead != null){
+        newOddList.next = newHead
+        newOddList = newOddList.next!!
+        if(newHead.next != null) {
+            newEvenList.next = newHead.next
+            newEvenList = newEvenList.next!!
+            newHead = newHead.next?.next
+        }else{
+            break
+        }
+    }
+    newEvenList.next = null
+    newOddList.next = evenList.next
+    return oddList.next
 }
