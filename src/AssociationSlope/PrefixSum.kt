306c8d6cd8249bf5e52f046bb000961c7eaeb53c
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-21 22:08:42 -0500

    Solved PrefixSum Q2

--- src/AssociationSlope/PrefixSum.kt
+++ src/AssociationSlope/PrefixSum.kt
@@ -3,10 +3,34 @@ package AssociationSlope
 fun largestAltitude(gain: IntArray): Int {
     var current = 0
     var max = 0
-    for (i in gain){
+    for (i in gain) {
         current += i
-        if (current> max)
+        if (current > max)
             max = current
     }
     return max
+}
+
+fun minSubarray(nums: IntArray, p: Int): Int {
+    val totalRem = nums.map { it.toLong() }.sum() % p
+    if (totalRem == 0L) return 0
+
+    val lastSeen = HashMap<Long, Int>()
+    lastSeen[0L] = -1
+
+    var prefixRem = 0L
+    var minLen = nums.size
+
+    for (i in nums.indices) {
+        prefixRem = (prefixRem + nums[i]) % p
+
+        val target = (prefixRem - totalRem + p) % p
+        lastSeen[target]?.let { j ->
+            minLen = minOf(minLen, i - j)
+        }
+
+        lastSeen[prefixRem] = i
+    }
+
+    return if (minLen < nums.size) minLen else -1
 }
