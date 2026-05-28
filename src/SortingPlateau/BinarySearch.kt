da1e4a35db7efcc9e97ba827acecebc7db9146db
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-28 17:14:59 -0500

    Solved BinarySearchQ2

--- src/SortingPlateau/BinarySearch.kt
+++ src/SortingPlateau/BinarySearch.kt
@@ -1,17 +1,38 @@
 package SortingPlateau
 
+
 fun peakIndexInMountainArray(arr: IntArray): Int {
     fun binarySearch(left: Int, right: Int): Int {
         var mid = (left + right) / 2
         if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
             return mid
-        if (arr[mid]>arr[mid-1]){
+        if (arr[mid] > arr[mid - 1]) {
             return binarySearch(mid, right)
-        }else{
+        } else {
             return binarySearch(left, mid)
         }
         return -1 //shouldn't happen
     }
-    return binarySearch(0, arr.size-1)
+    return binarySearch(0, arr.size - 1)
 
+}
+
+fun search(nums: IntArray, target: Int): Int {
+    fun binarySearchWithTarget(left: Int, right: Int): Int {
+        var mid = (left + right) / 2
+
+        if (nums[mid] == target)
+            return mid
+        if (left == right || left > right)
+            return -1
+        if (nums[mid] < target) {
+
+            return binarySearchWithTarget(mid+1, right)
+        } else {
+
+            return binarySearchWithTarget(left, mid-1)
+        }
+        return -1 //shouldn't happen
+    }
+    return binarySearchWithTarget(0, nums.size - 1)
 }
