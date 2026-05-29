5c166c08e1b127ed4d46b402a5b1c5ffdb55c058
Eraqi <mahmouderaqi225@yahoo.com>
2026-05-28 20:12:26 -0500

    Solved BinarySearch Q4

--- src/SortingPlateau/BinarySearch.kt
+++ src/SortingPlateau/BinarySearch.kt
@@ -29,31 +29,61 @@ fun search(nums: IntArray, target: Int): Int {
             return -1
         if (nums[mid] < target) {
 
-            return binarySearchWithTarget(mid+1, right)
+            return binarySearchWithTarget(mid + 1, right)
         } else {
 
-            return binarySearchWithTarget(left, mid-1)
+            return binarySearchWithTarget(left, mid - 1)
         }
         return -1 //shouldn't happen
     }
     return binarySearchWithTarget(0, nums.size - 1)
 }
+
 fun judgeSquareSum(c: Int): Boolean {
 
     var right = sqrt(c.toDouble()).toLong()
     var left = 1
-    while (left>=right){
+    while (left >= right) {
         val leftSquared = left * left
-        val rightSquared = right* right
+        val rightSquared = right * right
         val res = leftSquared + rightSquared
         if (res == c.toLong())
             return true
-        if (res > c.toDouble()){
+        if (res > c.toDouble()) {
             left--
-        }else{
+        } else {
             right++
         }
     }
     return false
 
+}
+
+fun search2(nums: IntArray, target: Int): Int {
+    fun binarySearch(left: Int, right: Int): Int {
+        if (left > right)
+            return -1
+        val mid = (left + right) / 2
+        val current = nums[mid]
+        if (current == target)
+            return mid
+
+
+        //left side sorted
+        if (current >= nums[left]) {
+            if (target < current && target >= nums[left]) {
+                return binarySearch(left, mid - 1)
+            } else {
+                return binarySearch(mid + 1, right)
+            }
+        } else {
+            if (target > current && target <= nums[right]) {
+                return binarySearch(mid + 1, right)
+            }
+            return binarySearch(left, mid - 1)
+        }
+
+
+    }
+    return binarySearch(0, nums.size - 1)
 }
