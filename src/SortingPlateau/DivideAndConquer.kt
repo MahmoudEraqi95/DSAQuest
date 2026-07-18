4257bcaba9c1ce1314b971a071179f9bdde386b4
Eraqi <mahmouderaqi225@yahoo.com>
2026-07-18 13:26:09 -0500

    Solved Divide And Conquer Q3

--- src/SortingPlateau/DivideAndConquer.kt
+++ src/SortingPlateau/DivideAndConquer.kt
@@ -26,32 +26,81 @@ fun beautifulArray(n: Int): IntArray {
 val inorderIndex = HashMap<Int, Int>()
 var postOrderPointer: Int = 0
 fun buildTree(inorder: IntArray, postorder: IntArray): TreeNode? {
-    for(i in inorder.indices){
+    for (i in inorder.indices) {
         inorderIndex[inorder[i]] = i
     }
     postOrderPointer = postorder.lastIndex
     return build(postorder, 0, inorder.lastIndex)
 }
-fun build(postorder: IntArray, leftIndex:Int, rightIndex:Int):TreeNode?{
-    if(leftIndex>rightIndex){
+
+fun build(postorder: IntArray, leftIndex: Int, rightIndex: Int): TreeNode? {
+    if (leftIndex > rightIndex) {
         return null
     }
     val rootValue = postorder[postOrderPointer--]
     val rootNode = TreeNode(rootValue)
     val rootIndex = inorderIndex[rootValue]!!
-    rootNode.right = build(postorder, rootIndex+1, rightIndex)
-    rootNode.left = build(postorder, leftIndex, rootIndex-1)
+    rootNode.right = build(postorder, rootIndex + 1, rightIndex)
+    rootNode.left = build(postorder, leftIndex, rootIndex - 1)
     return rootNode;
 }
-fun reversePairs(nums: IntArray): Int {
+
+fun reversePairsWithTLE(nums: IntArray): Int {
     var result = 0
-    for( i in nums.indices){
+    for (i in nums.indices) {
         val currentI = nums[i]
-        for (j in i+1..nums.size-1){
-            val doubleOfJ: Long = 2* nums[j].toLong()
-            if (currentI> doubleOfJ)
+        for (j in i + 1..nums.size - 1) {
+            val doubleOfJ: Long = 2 * nums[j].toLong()
+            if (currentI > doubleOfJ)
                 result++
         }
     }
     return result
+}
+
+fun reversePairs(nums: IntArray): Int {
+    return mergeSort(nums, 0, nums.size - 1)
+}
+
+private fun mergeSort(nums: IntArray, left: Int, right: Int): Int {
+    if (left >= right) return 0
+
+    val mid = left + (right - left) / 2
+
+    var count = mergeSort(nums, left, mid)
+    count += mergeSort(nums, mid + 1, right)
+
+    var j = mid + 1
+    for (i in left..mid) {
+        while (j <= right && nums[i].toLong() > 2L * nums[j].toLong()) {
+            j++
+        }
+        count += j - (mid + 1)
+    }
+    val temp = IntArray(right - left + 1)
+    var i = left
+    j = mid + 1
+    var k = 0
+
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j]) {
+            temp[k++] = nums[i++]
+        } else {
+            temp[k++] = nums[j++]
+        }
+    }
+
+    while (i <= mid) {
+        temp[k++] = nums[i++]
+    }
+
+    while (j <= right) {
+        temp[k++] = nums[j++]
+    }
+
+    for (idx in temp.indices) {
+        nums[left + idx] = temp[idx]
+    }
+
+    return count
 }
